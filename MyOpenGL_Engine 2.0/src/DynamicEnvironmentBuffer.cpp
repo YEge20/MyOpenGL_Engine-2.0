2a4403b3f3dff4ec809a527acb180a5e6cec60b7
烨哥 <1519267583@qq.com>
2024-11-26 13:48:14 +0800

    add some useful tools for debuging

--- MyOpenGL_Engine 2.0/src/DynamicEnvironmentBuffer.cpp
+++ MyOpenGL_Engine 2.0/src/DynamicEnvironmentBuffer.cpp
@@ -16,11 +16,10 @@ DynamicEnvironmentBuffer::DynamicEnvironmentBuffer(int size, Param param)
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	}
-	if (m_param == NEARLIST_MODE)
+	else if (m_param == NEARLIST_MODE)
 	{
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
 	}
 
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_DynamicEnvironmentTexture, 0);
@@ -49,11 +48,83 @@ void DynamicEnvironmentBuffer::setGLTextureSlot(int GL_Texture_slot)
 	glBindTexture(GL_TEXTURE_2D, m_DynamicEnvironmentTexture);
 }
 
+void DynamicEnvironmentBuffer::TestShader()
+{
+	ShaderFileType src;
+	src.VertexShader = R"(
+	#version 330 core
+
+	layout(location = 0) in vec4 position;
+	layout(location = 1) in vec2 uv;
+
+	uniform mat4 u_MVP;
+
+	out vec2 texcoord;
+	void main()
+	{
+		texcoord = uv;
+		gl_Position = u_MVP * position;
+	}
+	)";
+	src.FragmentShader = R"(
+	#version 330 core
+
+	layout(location = 0) out vec4 color;
+
+	uniform sampler2D u_texture;
+
+	in vec2 texcoord;
+	void main()
+	{
+		color = texture(u_texture, texcoord);
+	}
+	)";
+	if (m_shader_intalize == false)
+	{
+		m_test_shader.reset(new Shader(""));
+		m_test_shader->InsideShader(src.VertexShader, src.FragmentShader);
+	}
+}
+
+void DynamicEnvironmentBuffer::TestShaderBind()
+{
+	m_test_shader->bind();
+}
+
+void DynamicEnvironmentBuffer::SetTestShaderMVP(glm::mat4 MVP)
+{
+	m_test_shader->setuniformMat4f("u_MVP", MVP);
+}
+
 void DynamicEnvironmentBuffer::Bind()
 {
 	glViewport(0, 0, m_width, m_height);
 	glBindFramebuffer(GL_FRAMEBUFFER, m_FBO);
+	setGLTextureSlot(0);
 }
+
+glm::mat4 DynamicEnvironmentBuffer::Caculate_Ortho_Project(float width, float height, float farplane)
+{
+	float x, y;
+	x = width / 2;
+	y = height / 2;
+	glm::mat4 project = glm::ortho(-x, x, -y, y, 0.1f, farplane);
+	return project;
+}
+
+glm::mat4 DynamicEnvironmentBuffer::Caculate_Perspective_Project(float fov_angle, float width, float height, float farplane)
+{
+	glm::mat4 project = glm::perspective(glm::radians(fov_angle), width / height, 0.1f, farplane);
+	return project;
+}
+
+glm::mat4 DynamicEnvironmentBuffer::Caculate_Camera(glm::vec3 Cam_Pos, glm::vec3 Cam_Fro)
+{
+	glm::vec3 lookat = Cam_Pos + glm::normalize(Cam_Fro);
+	glm::mat4 Camera = glm::lookAt(Cam_Pos, lookat, glm::vec3(0.0f, 1.0f, 0.0f));
+	return Camera;
+}
+
 
 void DynamicEnvironmentBuffer::UnBind()
 {
--- MyOpenGL_Engine 2.0/src/DynamicEnvironmentBuffer.h
+++ MyOpenGL_Engine 2.0/src/DynamicEnvironmentBuffer.h
@@ -2,6 +2,10 @@
 #include<iostream>
 #include<Windows.h>
 #include<gl/glew.h>
+#include<memory>
+#include"shader.h"
+#include"glm/glm.hpp"
+#include"glm/gtc/matrix_transform.hpp"
 //这是设置帧缓冲方法，用于生成某个场景画面的贴图
 enum Param
 {
@@ -11,19 +15,39 @@ enum Param
 class DynamicEnvironmentBuffer
 {
 public:
-	//设置贴图的尺寸，最好与要渲染的平面的大小相同,param是设置贴图的过滤模式
+	//初始化：size设置贴图的分辨率，param是设置贴图的过滤模式
 	DynamicEnvironmentBuffer(int size, Param param = LINEAR_MODE);
 	~DynamicEnvironmentBuffer();
-	//绑定到对应的GL_TEXTURE插槽上去
-	void setGLTextureSlot(int GL_Texture_slot);
+
 	//在要开始生成的场景前绑定,会改变视口的大小
 	void Bind();
-	//结束场景贴图的渲染，请手动调用glviewport()手动复原回原来的视口大小！
+
+	////使用该API推荐用这里提供的自动计算所需要的视图矩阵和相机矩阵////
+	/*计算正交矩阵视图，width为渲染对象的宽度，height为渲染对象高度,farplane为能看到最远的距离*/
+	glm::mat4 Caculate_Ortho_Project(float width, float height, float farplane);
+	/*计算透视矩阵，fov_angle为视角夹角，单位是度，width是渲染对象的宽度，height为渲染对象高度,farplane为能看到最远的距离*/
+	glm::mat4 Caculate_Perspective_Project(float fov_angle, float width, float height, float farplane);
+	/*计算相机矩阵，Cam_Pos是观察时的位置，Cam_Fro是观察的方向（例如方向是沿着x轴的正方向，那就可以填入坐标glm::vec3(1.0f,0.0f,0.0f)）*/
+	glm::mat4 Caculate_Camera(glm::vec3 Cam_Pos, glm::vec3 Cam_Fro);
+
+	//结束渲染后请务必使用解绑并恢复到正常渲染，请手动调用glviewport()手动复原回原来的视口大小！
 	void UnBind();
+	//绑定到即将使用对应的GL_TEXTURE插槽上去
+	void setGLTextureSlot(int GL_Texture_slot);
+
+	////以下的三个函数提供测试你的动态立方贴图的样子，使用顺序如下////
+	/*先初始化着色器（只执行一次）：*/
+	void TestShader();
+	/*然后绑定着色器（在循环中执行）：*/
+	void TestShaderBind();
+	/*最后设置MVP（在循环中更新）：*/
+	void SetTestShaderMVP(glm::mat4 MVP);
 private:
 	unsigned int m_FBO;
 	unsigned int m_DynamicEnvironmentTexture;
 	unsigned int m_RBO;
 	int m_width, m_height;
 	Param m_param;//贴图的过滤模式
+	std::shared_ptr<Shader> m_test_shader;
+	bool m_shader_intalize = false;
 };
